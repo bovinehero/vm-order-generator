e8ff426bef51fc2eecf23f68fe6864744be8c660
Bovine Hero <garygrant79@yahoo.co.uk>
2022-11-06 10:31:36 +0000

    Adding base function placeholders

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,95 @@
  */
 
 console.log("JavaScript Connected!")
+
+/**
+ * Loads the help text when ? icon is clicked
+ */
+function loadHelp() {
+
+}
+
+/**
+ * Loads the order form modal when request button is clicked
+ */
+function launchModal() {
+
+}
+
+/**
+ * Fetches the available configurations for the option: CPU
+ */
+function fetchCPUOptions() {
+
+}
+
+/**
+ * Fetches the available configurations for the option: Memory
+ */
+function fetchMemoryOptions() {
+
+}
+
+/**
+ * Fetches the available configurations for the option: Disk
+ */
+function fetchDiskOptions() {
+
+}
+
+/**
+ * Fetches the available configurations for the option: Operating Systems
+ */
+function fetchOSOptions() {
+
+}
+
+/**
+ * Fetches the available configurations for the option: Environments
+ */
+function fetchEnvironmentOptions() {
+
+}
+
+/**
+ * Fetches the available configurations for the option: Location
+ */
+function fetchLocationOptions() {
+
+}
+
+/**
+ * Clears the order form to allow the user to reset all inputs
+ */
+function clearOrder() {
+
+}
+
+/**
+ * Cancels order and return to the index page
+ */
+function cancelOrder() {
+
+}
+
+/**
+ * Submits order and return to the index page
+ */
+function submitOrder() {
+
+}
+
+/**
+ * Validates the order prioir to submit
+ */
+function validateOrder() {
+
+}
+
+/**
+ * writes a file for download
+ */
+function downloadOrder() {
+
+}
+
