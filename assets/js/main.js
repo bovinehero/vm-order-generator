4e8803753bc9e0281bb2ee1e92b82e7129d17baf
Bovine Hero <garygrant79@yahoo.co.uk>
2022-11-06 16:41:00 +0000

    Adding base modal functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,97 +1,118 @@
 /**
- * Main JavaScript for the site
- */
+* Main JavaScript for the site
+*/
 
 console.log("JavaScript Connected!")
 
+// Get the modal
+let modal = document.getElementById("vmRequestModal");
+
+// Get the button that opens the modal
+let btn = document.getElementById("vmRequest");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("closeRequest")[0];
+
 /**
- * Loads the help text when ? icon is clicked
+ * Loads the help text
  */
 function loadHelp() {
 
 }
 
 /**
- * Loads the order form modal when request button is clicked
+ * Loads the order form modal
  */
 function launchModal() {
-
+    modal.style.display = "block";
 }
 
 /**
- * Fetches the available configurations for the option: CPU
- */
+* Fetches the available configurations for the option: CPU
+*/
 function fetchCPUOptions() {
 
 }
 
 /**
- * Fetches the available configurations for the option: Memory
- */
+* Fetches the available configurations for the option: Memory
+*/
 function fetchMemoryOptions() {
 
 }
 
 /**
- * Fetches the available configurations for the option: Disk
- */
+* Fetches the available configurations for the option: Disk
+*/
 function fetchDiskOptions() {
 
 }
 
 /**
- * Fetches the available configurations for the option: Operating Systems
- */
+* Fetches the available configurations for the option: Operating Systems
+*/
 function fetchOSOptions() {
 
 }
 
 /**
- * Fetches the available configurations for the option: Environments
- */
+* Fetches the available configurations for the option: Environments
+*/
 function fetchEnvironmentOptions() {
 
 }
 
 /**
- * Fetches the available configurations for the option: Location
- */
+* Fetches the available configurations for the option: Location
+*/
 function fetchLocationOptions() {
 
 }
 
 /**
- * Clears the order form to allow the user to reset all inputs
- */
+* Clears the order form to allow the user to reset all inputs
+*/
 function clearOrder() {
 
 }
 
 /**
- * Cancels order and return to the index page
- */
+* Cancels order and return to the index page
+*/
 function cancelOrder() {
-
+    modal.style.display = "none";
 }
 
 /**
- * Submits order and return to the index page
- */
+* Submits order and return to the index page
+*/
 function submitOrder() {
 
 }
 
 /**
- * Validates the order prioir to submit
- */
+* Validates the order prioir to submit
+*/
 function validateOrder() {
 
 }
 
 /**
- * writes a file for download
- */
+* writes a file for download
+*/
 function downloadOrder() {
 
 }
 
+// When the user clicks on Request Button open the modal
+btn.addEventListener("click", launchModal);
+
+// When the user clicks on <span> (x), close the modal
+span.addEventListener("click", cancelOrder);
+
+// When the user clicks anywhere outside of the open modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    cancelOrder();
+  }
+}
